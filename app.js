1a8e8bd076f530bd9d97012ba70293bc31247493
Sergii <anisimovsergii@gmail.com>
2024-11-15 22:37:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const carouselElements = document.querySelectorAll(".splide");
+
+  carouselElements.forEach((carouselElement) => {
+    const splide = new Splide(carouselElement, {
+      type: "loop",
+      drag: true,
+      autoWidth: true,
+      gap: 30,
+      pagination: false,
+      arrows: false,
+      autoScroll: {
+        speed: 1,
+        pauseOnHover: true,
+      },
+    });
+    splide.mount(window.splide.Extensions);
+  });
